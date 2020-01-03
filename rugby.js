a79b57cb7aa8090c571f8519ee9aa8dbeec44545
Manav Misra <manavm@visionwebsoft.com>
2020-01-02 22:08:30 -0600

    :sparkles: Convert heights.

--- rugby.js
+++ rugby.js
@@ -1,2 +1,6 @@
 const heightCm = document.querySelector("#height-cm");
 const heightFt = document.querySelector("#height-ft");
+
+heightCm.addEventListener("input", function() {
+  heightFt.value = this.value * 0.032808;
+});
