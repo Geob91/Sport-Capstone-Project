51989f17f099b620af41dbf91fe96b1e91a110ea
George Blezard <gblezard91@gmail.com>
2020-01-07 11:42:03 -0600

    add event listeners on index js file.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 import { Header, Nav, Main, Footer } from "./components";
 
-console.log(Header, Footer, Main, Nav);
+import { Header, Nav, Main, Footer } from "./components";
+
+//console.log(Header, Footer, Main, Nav);
+
+const state = {
+  Home: {
+    heading: "Home"
+  },
+  Form: {
+    heading: "Form"
+  },
+  Blog: {
+    heading: "Blog"
+  },
+  Gallery: {
+    heading: "Gallery"
+  }
+};
+
+function render(st = state.Home) {
+  document.querySelector("#root").innerHTML = `
+${Header(st)}
+${Nav(st)}
+${Main(st)}
+${Footer(st)}`;
+
+  document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      render(state[event.target.textContent]);
+    });
+  });
+}
+
+render();
+
