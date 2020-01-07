337fac4e581dea68f994e3f1f6170918f347076d
George Blezard <gblezard91@gmail.com>
2020-01-06 19:52:44 -0600

    add new components

--- components/index.js
+++ components/index.js
@@ -2,5 +2,3 @@ export { default as Header } from "./Header";
 export { default as Nav } from "./Nav";
 export { default as Main } from "./Main";
 export { default as Footer } from "./Footer";
-
-console.log(Header, Footer, Main, Nav);
