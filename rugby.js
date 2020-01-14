46ef7853b78f14a147b7bcf638104167794af6be
George Blezard <gblezard91@gmail.com>
2020-01-13 18:32:24 -0600

    add if else statements working with gaps

--- rugby.js
+++ rugby.js
@@ -299,32 +299,43 @@ const rugbyPlayers = [
 ];
 
 function findPosition(height, weight, speed) {
-  if (height <= 197 && => 192 && weight >= 250 && speed === "slow" ||  "very slow"){
+  if (
+    (height => 192 && height <= 197) &&
+    weight >= 254 &&
+    speed === "very slow"
+  ) {
     return "You should be in the front row.";
-
-  /*} else if (height > 198 && weight > 245 && speed != "very slow") {
+  } else if (height >= 198 && weight > 245 && speed === "very slow") {
     return "you could be a lock";
+  } else if (
+    height >= 179 &&
+    height <= 192 &&
+    (weight => 242 && weight <= 253) &&
+    speed === "slow"
+  ) {
+    return "you could be a loose forward";
+  } else if (height <= 169 && weight <= 190 && speed === "medium") {
+    return "You may be a scrum half";
+  } else if (
+    height >= 178 &&
+    height <= 192 &&
+    weight >= 190 &&
+    weight <= 200 &&
+    speed === "fast"
+  ) {
+    return "Lets put you in the backline depending on your skills you could be a fly half, center or even a full back.";
+  } else if (
+    height > 170 &&
+    height < 188 &&
+    weight < 210 &&
+    speed === "very fast"
+  ) {
+    return "Because of your speed I think you are a winger";
+  } else {
+    return "Lets get you on a training pitch to really find out.";
   }
-    else if (height > 179 && < 192 && weight > 242 && < 253 && speed ==="slow" || "medium"){
-      return "you could be a loose forward";
-    }
-
-    else if (height < 169 && weight < 190 && speed !="very slow" || "slow"){
-      return "You may be a scrum half"
-    }
-
-    else if(height > 179 && < 192 && weight > 190 && < 200 && speed ==="fast" || "very fast"){
-      return "Lets put you in the backline depending on your skills you could be a fly half, center or even a full back."
-    }
-    else if (height > 170 && < 188 &&  weight < 210 && speed ==="very fast"){
-      return"Because of your speed I think you are a winger"
-    }
-    else{
-      return "Lets get you on a training pitch to really find out."
-    }
-    */
 }
-console.log(findPosition(200, 241, "slow"));
+console.log(findPosition(199, 265, "slow"));
 
 // console.log(rugbyPlayers);
 // function getCountry(name, Nationality) {
