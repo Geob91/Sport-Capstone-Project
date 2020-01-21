bab1db2d3f00657e7a8789afc2ec3e8c3fa704d8
George Blezard <gblezard91@gmail.com>
2020-01-20 19:52:41 -0600

    add new font color and gallery style

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,7 +2,7 @@ export default `
 
 
 <h2>About the Site</h2>
-<p>This site has been built to share my love of Rugby with you, my inspiration came about in the most recent <a href ="https://www.rugbyworldcup.com/" target="_blank">Rugby world cup</a>and how little people knew of the sport. My goal here is to get gather some more interest and possibly get some more people playing the sport. If you are interested in playing there is a short form to fill out to see what position you could play.To really get a good understanding of the position you are just going to have to try it out!&nbsp;<strong>Be warned! Rugby is not for the faint of heart!</strong>.So if the idea of tackling somebody or being tackled in the mud doesn't sound like fun to you this may not be your sport.</p>
+<p>This site has been built to share my love of Rugby with you, my inspiration came about in the most recent <a href ="https://www.rugbyworldcup.com/" target="_blank">Rugby world cup </a>and how little people knew of the sport. My goal here is to get gather some more interest and possibly get some more people playing the sport. If you are interested in playing there is a short form to fill out to see what position you could play.To really get a good understanding of the position you are just going to have to try it out!&nbsp;<strong>Be warned! Rugby is not for the faint of heart!</strong>.So if the idea of tackling somebody or being tackled in the mud doesn't sound like fun to you this may not be your sport.</p>
 
 <h3>The History of Rugby</h3>
 <p>Rugby is said to have originated in Rugby school, Warwickshire, England in the year 1823. During a game of football William Webb Ellis picked up a ball and started running with it. Though this is a myth the world cup is named the William Web Ellis cup. The first world cup was only played in 1987 with New Zealand the ultimate winners. The world cup is played every 4 years. New Zealand, South Africa have won it 3 times, Australia 2 and England the only northern hemisphere team to win it with 1.
@@ -14,7 +14,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
 <h3>News Stories Around the World</h3>
 
 <div class="flex" style="overflow: hidden;">
-  <div>
+  <div class="rugbypic">
     <a href="https://www.planetrugby.com/planet-rugbys-team-of-the-year-7/">
       <figure>
         <img src="https://d3gbf3ykm8gp5c.cloudfront.net/content/uploads/2019/12/18113034/Planet-Rugby-Readers-Team-of-2019-PA.jpg" />
@@ -23,7 +23,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
     </a>
   </div>
 
-  <div>
+  <div class="rugbypic">
     <a href="https://www.planetrugby.com/tendai-mtawarira-signs-for-american-side-old-glory-dc">
       <figure>
         <img src="https://d3gbf3ykm8gp5c.cloudfront.net/content/uploads/2019/12/18173925/Tendair-Mtawarira-smiling-South-Africa-PA.jpg" alt="random image from unsplash" />
@@ -32,7 +32,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
     </a>
   </div>
 
-  <div>
+  <div class="rugbypic">
     <a href="https://lasevensrugby.com/">
       <figure>
         <img src="https://i.axs.com/2019/10/2020-l-a-sevens-tickets_02-29-20_17_5d9d2d85bbdb4.jpg" />
@@ -41,7 +41,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
     </a>
   </div>
 
-  <div>
+  <div class="rugbypic">
     <a href="https://www.planetrugby.com/planet-rugby-quiz-2019-part-one/">
       <figure>
         <img src="https://d3gbf3ykm8gp5c.cloudfront.net/content/uploads/2019/12/21190220/Wales-centre-Jonathan-Davies-hands-on-head-PA.jpg">
@@ -50,7 +50,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
     </a>
   </div>
 
-  <div>
+  <div class="rugbypic">
     <a href="https://www.usa.rugby/2019/12/rugby-americas-announces-changes-to-mens-xv-competition-structure-to-maximize-growth-throughout-region/">
       <figure>
         <img src="https://wpcontent-usarugby.netdna-ssl.com/uploads/2019/12/ARC-Generic-1199x674.png">
@@ -59,7 +59,7 @@ The most recent winners of the world cup are South Africa where they beat Englan
     </a>
   </div>
 
-  <div>
+  <div class="rugbypic">
     <a href="https://www.bbc.com/sport/rugby-union/50519816">
       <figure>
         <img src="https://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/A1E9/production/_109794414_victoryparademontage.jpg" />
--- components/views/Rules.js
+++ components/views/Rules.js
@@ -6,13 +6,13 @@ This sections is about the rules of rugby. Like any game there are a lot of rule
 
 <ol id= "rules">
 <li>There are 15 people on each side.</li>
-<li>To score points you have to put the ball down on the other end of the pitch for 5 pts and kick for another 2pts</li>
-<li>The ball must always be passed backwards</li>
-<li>If you drop the ball or knock it on the other team get possesion in a scrum</li>
+<li>To score points you have to put the ball down on the other end of the pitch for 5 pts and kick for another 2pts.</li>
+<li>The ball must always be passed backwards.</li>
+<li>If you drop the ball or knock it on the other team get possesion in a scrum.</li>
 <li>You must tackle people below the shoulder.</li>
 <li>You cannot tackle people in the air.</li>
 <li>No swearing at the referee...they will send you off.</li>
-<li>If you consistently foul then you will get a yellow card which is 10 mins off the field </li>
+<li>If you consistently foul then you will get a yellow card which is 10 mins off the field.</li>
 <li>If you do something dangerous such as a high tackle or tackle in the air you may get a red card which is completely sent off.</li>
 <li>Have fun and respect all in the game.</li>
 </ol>
