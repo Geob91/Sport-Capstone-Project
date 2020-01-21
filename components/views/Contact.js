4b1f16440d2856fb1f247fdfa8cc7802e4b933fe
George Blezard <gblezard91@gmail.com>
2020-01-21 11:56:23 -0600

    fix quotes around form name

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,6 +1,6 @@
 export default `
 <div>
-<form method="POST" data-netlify="true" name ="contact class ="contact" id="contactForm">
+<form method="POST" data-netlify="true" name="contact" class="contact" id="contactForm">
   <div>
     <label for= "First Name">First Name</label>
     <input type="text" id="fname" name="firstname" placeholder="First Name">
